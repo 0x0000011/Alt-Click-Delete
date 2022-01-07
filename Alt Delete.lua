a8e8af01d45ea24dc93d430e945ff7cd4be56d28
0x0000011 <90667396+0x0000011@users.noreply.github.com>
2022-01-07 14:42:25 +0300

    Add files via upload

---
+++ Alt Delete.lua
@@ -0,0 +1,13 @@
+local player = game:GetService("Players").LocalPlayer
+local mouse = player:GetMouse()
+local userInputService = game:GetService("UserInputService")
+
+local function main()
+	if userInputService:IsKeyDown(Enum.KeyCode.LeftAlt) then
+		if mouse.Target then
+			mouse.Target:Destroy()
+		end
+	end
+end
+
+mouse.Button1Down:Connect(main)
